cc58e1a5f69a256ff7cabce70b31a821621fcb3e
hperezmasci <hernan.perez@gmail.com>
2021-09-09 10:09:00 -0300

    clase 10

---
+++ clase10/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.use(express.static('public'));
+
+const PORT = 8080;
+const server = app.listen(PORT, ()=>{
+    console.log('Servidor HTTP escuchando en el puerto', server.address().port);
+});
+server.on('error', error=>console.log('Error en servidor', error));
