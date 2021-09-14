c03b4c6f287359ab6915d4bf5aca8f1003e4dea5
hperezmasci <hernan.perez@gmail.com>
2021-09-14 12:00:53 -0300

    clase 11

--- clase11/broadcast.js
+++ clase11/broadcast.js
@@ -10,7 +10,7 @@ const io = new IOServer(httpServer)     // IO server sobre el http server
 const PORT = 3030;
 const mensajes = [];
 
-app.use(express.static('./public'));
+app.use(express.static('./public-broadcast'));
 
 httpServer.listen(PORT, () => {
     console.log('Servidor HTTP escuchando en el puerto', PORT);
@@ -19,14 +19,8 @@ httpServer.listen(PORT, () => {
 io.on('connection', (socket) => {
     console.log('Usuario conectado')
 
-    socket.emit('mi mensaje', 'este es mi mensaje desde el servidor')
-
-    socket.on('notificacion', data => {
-        console.log(data)
+    socket.on('texto', (data) => {
+        io.sockets.emit('broadcast', data)
     })
 
-    socket.on('mensaje', (data) => {
-        mensajes.push({ socketid: socket.id, mensaje: data })
-        io.sockets.emit('mensajes', mensajes)
-    })
 })
