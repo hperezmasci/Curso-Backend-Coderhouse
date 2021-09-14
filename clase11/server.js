063a01bb669c33072d26fc1e3805d37ccf0ea8b6
hperezmasci <hernan.perez@gmail.com>
2021-09-14 11:07:55 -0300

    desafio clase 10 profe + clase 11

---
+++ clase11/server.js
@@ -0,0 +1,21 @@
+const { application } = require('express')
+const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const app = express()
+const httpServer = new HttpServer(app)  // nuevo server http, reemplazando el de express
+const io = new IOServer(httpServer)     // IO server sobre el http server
+
+const PORT = 3030;
+const mensajes = [];
+
+app.use(express.static('./public'));
+
+httpServer.listen(PORT, () => {
+    console.log('Servidor HTTP escuchando en el puerto', PORT);
+});
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado')
+})
