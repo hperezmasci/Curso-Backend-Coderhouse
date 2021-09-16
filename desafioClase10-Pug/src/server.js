0e616e2f85ee28e708dda1c2ab385f0588a26343
hperezmasci <hernan.perez@gmail.com>
2021-09-16 19:48:36 -0300

    pug

---
+++ desafioClase10-Pug/src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import Contenedor from './ContenedorAsync.js';
+
+const app = express()
+
+app.use(express.static('public'));
+
+const { Router } = express;
+const router = new Router();
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+const cont = new Contenedor("./data/productos.txt");
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await cont.getAll()
+        const listExists = products.length
+        res.render('getProducts.pug', {products, listExists})
+    }
+    catch (err) {throw new Error(`GET /productos: ${err}`)}
+})
+
+router.post('/', async (req, res) => {
+  try {
+      const product = req.body;
+      await cont.save(product)
+      res.redirect('/')
+  }
+  catch (err) {throw new Error(`POST /productos: ${err}`)}
+})
+
+app.use('/productos', router);
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
